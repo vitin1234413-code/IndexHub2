d1d688880ce85bbffd075da8e50927c6c7e39151
vitin1234413-code <vitin1234413@gmail.com>
2026-01-17 03:24:02 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/d0a539c139310db5106e457a4f3cc7009b28b6919d7ebdd1ad32cf4f28322ba0/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/43c737203a85de40c221e9bb8a1afa34c8179bdf4e430c210778bb005a4277f4/download"))()
